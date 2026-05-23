98ad030eebe0414f3e5f679bb54dca3e8e5ddfd9
Divyanshu Kumar Jha <divyanshukumarjha47@gmail.com>
2026-05-23 22:46:50 +0530

    Add dynamic background component

---
+++ frontend/src/components/DynamicBackground.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DynamicBackground = () => {
+  return (
+    <div className="dynamic-bg-container">
+      <div className="mesh-blob mesh-1"></div>
+      <div className="mesh-blob mesh-2"></div>
+      <div className="mesh-blob mesh-3"></div>
+      <div className="noise-overlay"></div>
+    </div>
+  );
+};
+
+export default DynamicBackground;
